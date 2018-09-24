035e123f68e4508712eb340ed4c0381a10aaa5df
Heidi Engstrom <hengstrom25@gmail.com>
2018-09-24 16:55:02 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,14 +12,14 @@ class PostsController < ApplicationController
 	end
 
   def create
-    @post = Post.new(post_params)
+    @post = Post.new(post_params(:title, :description))
     @post.save
     redirect_to post_path(@post)
   end
    
   def update
     @post = Post.find(params[:id])
-    @post.update(post_params)
+    @post.update(post_params(:title))
     redirect_to post_path(@post)
   end
    
@@ -28,6 +28,7 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :description)
   end
+  
 	def edit
 	  @post = Post.find(params[:id])
 	end
